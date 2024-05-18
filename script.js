4b95eb93f66f84d524de72547d93694f7fd1782c
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-05-18 17:19:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	constructor(){
+		this._make=make;
+		this._model=model;
+	};
+	getMakeModel(){
+		return (`${this._make} ${this._model}`);
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	constructor(){
+		this._make=make;
+		this._model=model;
+		this._topSpeed=topSpeed;
+	}
+	getTopSpeed(){
+		return this._topSpeed;
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
