99c8f4a41468729da0b9de5133660be8d22eca31
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-09-03 16:24:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,19 @@
-// // Complete the js code
-// function Car(make, model) {
-// 	constructor(){
-// 		this._make=make;
-// 		this._model=model;
-// 	};
-// 	getMakeModel(){
-// 		return (`${this._make} ${this._model}`);
-// 	}
-// }
-
-// function SportsCar(make, model, topSpeed) {
-// 	constructor(){
-// 		this._make=make;
-// 		this._model=model;
-// 		this._topSpeed=topSpeed;
-// 	}
-// 	getTopSpeed(){
-// 		return this._topSpeed;
-// 	}
-// }
-
-function Car(make, model) {
-    this._make = make;
-    this._model = model;
+let Car=function(make,model){
+	constructor(){
+		this.make=make;
+		this.model=model;
+	}
 }
-
-Car.prototype.getMakeModel = function () {
-    return `${this._make} ${this._model}`;
-};
-
-function SportsCar(make, model, topSpeed) {
-    Car.call(this, make, model); // Inherit properties from Car
-    this._topSpeed = topSpeed;
+Car.Prototype.getMakeModel=function(){
+	return this.make+" "+this.model;
 }
-
-SportsCar.prototype = Object.create(Car.prototype); // Inherit methods from Car
-SportsCar.prototype.constructor = SportsCar; // Fix constructor reference
-
-SportsCar.prototype.getTopSpeed = function () {
-    return this._topSpeed;
-};
-
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+let SportsCar=function(make,model,topSpeed){
+	constructor(){
+		this.make=make;
+		this.model=model;
+		this.topSpeed=topSpeed;
+	}
+}
+SportsCar.Prototype.getTopSpeed=function(){
+	return this.topSpeed;
+}
