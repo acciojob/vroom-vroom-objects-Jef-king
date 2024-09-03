3f52c0c3ba18559d3ee0b5486aee9292e0db93cf
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-09-03 16:37:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-function Car(make,model){
-	constructor(){
-		this.make=make;
-		this.model=model;
-	}
+// function Car(make,model){
+// 	constructor(){
+// 		this.make=make;
+// 		this.model=model;
+// 	}
+// }
+// Car.prototype.getMakeModel=function(){
+// 	return this.make+" "+this.model;
+// }
+// function SportsCar(make,model,topSpeed){
+// 	constructor(){
+// 		Car.call(this,make,model);
+// 		this.topSpeed=topSpeed;
+// 	}
+// }
+// SportsCar.prototype = Object.create(Car.prototype);
+// SportsCar.prototype.constructor = SportsCar;
+// SportsCar.prototype.getTopSpeed=function(){
+// 	return this.topSpeed;
+// }
+// Car constructor function
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
 }
-Car.prototype.getMakeModel=function(){
-	return this.make+" "+this.model;
+
+// Adding getMakeModel method to Car prototype
+Car.prototype.getMakeModel = function() {
+    return `${this.make} ${this.model}`;
+};
+
+// SportsCar constructor function
+function SportsCar(make, model, topSpeed) {
+    // Inherit properties from Car
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
-function SportsCar(make,model,topSpeed){
-	constructor(){
-		Car.call(this,make,model);
-		this.topSpeed=topSpeed;
-	}
-}
+
+// Inherit Car prototype methods
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
-SportsCar.prototype.getTopSpeed=function(){
-	return this.topSpeed;
-}
+
+// Adding getTopSpeed method to SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+    return this.topSpeed;
+};
+
+// Example usage
+const car = new SportsCar("Ferrari", "Testarossa", 200);
+console.log(car.getMakeModel()); // Output: Ferrari Testarossa
+console.log(car.getTopSpeed()); // Output: 200
